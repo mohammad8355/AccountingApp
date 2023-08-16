ad65ccfe5d4bd80f5fe295469fcedec77e2c46f3
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-08-16 13:30:47 -0700

    Accounting.Utility commit
    commit utility layer

---
+++ Accounting.Utillity/Components/Dateconvertor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace Accounting.Utillity.Components
+{
+   public static class Dateconvertor
+    {
+        public static string ToShamsi(this DateTime value)
+        {
+            PersianCalendar date = new PersianCalendar();
+
+            return date.GetYear(value) +"/"+ date.GetMonth(value).ToString("00")+"/" + date.GetDayOfMonth(value).ToString("00");
+        }
+        public static DateTime ToMiladi(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, new PersianCalendar());
+        }
+    }
+}
